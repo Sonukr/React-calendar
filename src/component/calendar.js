dbea7eefc340196498dd051d3bc07024a51d89b7
Sonukr <Sonuyadav9756@gmail.com>
2019-02-09 23:02:37 +0530

    initial app setup

---
+++ src/component/calendar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class CalendarProxy extends Component {
+  render() {
+    return (
+      <div>
+        Calendar
+      </div>
+    );
+  }
+}
+
+CalendarProxy.propTypes = {};
+
+export const Calendar =  CalendarProxy;
